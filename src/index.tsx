58454321a87ec71be7646b600823d1777c2ec148
Dejan Toteff <deyan8284@gmail.com>
2017-09-21 13:30:50 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 interface HelloProps { compiler: string; framework: string; }
 
-const Hello = (props: HelloProps) => <h1>Hello {props.compiler} and {props.framework}!</h1>
+const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>
 
 const id = 'react-container'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 const plugins = [
-  new CleanWebpackPlugin(['dist']),
-  new HtmlWebpackPlugin({
-    title: 'Foo'
-  }),
+  new CleanWebpackPlugin([ 'dist' ]),
+  new HtmlWebpackPlugin({ title : 'Foo' }),
   new webpack.HotModuleReplacementPlugin(),
 ]
 
 const devServer = {
-  contentBase: './dist',
-  hot: true
+  contentBase : './dist',
+  hot         : true,
 }
 
+const entry = [
+  'react-hot-loader/patch',
+  'webpack-dev-server/client?http://localhost:8080',
+  'webpack/hot/only-dev-server',
+//  'babel-polyfill',
+  './src/index.tsx',
+]
+
+//const entry = "./src/index.tsx",
+
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    plugins: plugins,
+  entry  : entry,
+  output : {
+    filename : 'bundle.js',
+    path     : __dirname + '/dist',
+  },
+  plugins : plugins,
 
-    // devtool: "source-map",
+  //devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve : { extensions : [ '.ts', '.tsx', '.js', '.json' ] },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+  module : {
+    rules : [
+      {
+        test   : /\.tsx?$/,
+        loader : 'awesome-typescript-loader',
+      },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-};
+      //All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      // {
+      //   enforce : 'pre',
+      //   test    : /\.js$/,
+      //   loader  : 'source-map-loader',
+      // },
+    ],
+  },
+}
