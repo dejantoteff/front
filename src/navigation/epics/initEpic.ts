3431ad337e53a715f252e48a74011dbd87b4a558
Dejan Toteff <deyan8284@gmail.com>
2017-11-28 03:59:48 +0200

    chore@small: remove logs

--- src/navigation/epics/initEpic.ts
+++ src/navigation/epics/initEpic.ts
@@ -14,13 +14,13 @@ const getData$ = Observable.ajax(requestInput)
 
 type Delay = (ms: number) => Promise<string>
 
-const doa: Delay = (x) => new Promise(resolve =>{
-  console.log('as');
+const work: Delay = (x) => new Promise(resolve =>{
+  console.log('work');
   
   resolve('1')
 })
-const getDatax$ = Observable.fromPromise(doa(1))
-// const getDatax$ = Observable.fromPromise(delay(1000))
+
+const getDatax$ = Observable.fromPromise(work(1))
 
 export const initEpic = (
   action$: ActionsObservable<Init>,
@@ -30,14 +30,10 @@ export const initEpic = (
   action$
     .ofType(INIT)
     .concatMap(action => {
-      console.log(5621)
       return new Observable(observer => {
-        console.log(5)
 
-        // getRequest(requestInput).subscribe(({ response }) => {
-        // getData$.subscribe(({ response }) => {
         getDatax$.subscribe(response => {
-          console.log('initEpi66c88')
+          console.log('SUCCESS')
 
           observer.next({ type: DATA_READY, payload: response })
           observer.complete()
