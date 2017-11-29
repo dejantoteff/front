57cfbac9d49e16bf0cd2949381a78f92a8663dc2
Dejan Toteff <deyan8284@gmail.com>
2017-11-29 22:04:53 +0200

    test@UI: complete first example e2e test

---
+++ src/choose_word/e2e.spec.ts
@@ -0,0 +1,14 @@
+import { initPuppeteer } from 'init-puppeteer'
+import { delay } from 'rambdax'
+test('', async () => {
+  const { browser, page } = await initPuppeteer({
+    headless: false,
+    url: 'https://ilearnsmarter.com/',
+  })
+
+  await delay(3000)
+  await browser.close()
+  expect(
+    true,
+  ).toBeTruthy()
+})
