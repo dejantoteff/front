3872363e659fd56b55423330e1b92335e5b46c1b
Dejan Toteff <deyan8284@gmail.com>
2018-02-26 13:37:09 +0200

    feat@stop: answer in write.sentence

--- src/write_sentence/styled/input.tsx
+++ src/write_sentence/styled/input.tsx
@@ -1,3 +1,4 @@
+import { darkblue, green } from 'colors'
 import styled from 'styled-components'
 import { CenteredItem, height } from '../../_styled/grid'
 
@@ -9,9 +10,11 @@ export const Input = styled.div`
   padding-top: ${0.1 * height}vh;
   
   input {
+    caret-color: ${green};
     height: ${0.7 * height}vh;
     font-size: ${0.6 * height}vh;
     text-align: center;
-    width: 100%;
+    width: auto;
+    box-shadow: 3px 3px 3px ${darkblue};
   }
 `
