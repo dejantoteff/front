5402cda96b6db668e32884e0813eb6ba5de072ab
Dejan Toteff <deyan8284@gmail.com>
2018-03-07 10:23:56 +0200

    chore: use jest files from server-helpers

--- files/jest/testSetup.js
+++
@@ -1,9 +0,0 @@
-require('env')('special')
-const enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
-
-enzyme.configure({ adapter: new Adapter() });
-
-// No need to console.log
-// Use console.warn to debug if needed
-console.log = () => {}
