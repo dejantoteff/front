52ac9f9b6e38b2d86b049e57bffb9bef9ac7ef97
Dejan Toteff <deyan8284@gmail.com>
2018-03-09 10:25:31 +0200

    feat: glue.related

---
+++ src/_helpers/glueRelated.spec.ts
@@ -0,0 +1,25 @@
+import { glueRelated } from './glueRelated'
+
+test('', () =>{
+  const input = [
+    "foo",
+    "bar",
+    "baz",
+    "grandeveour",
+    "obnoixnousness",
+    "when i leave my proven record",
+    "loving yourself is hard when you suck",
+    "that is precicely the point you don't jump unless",
+    "having trouble recreating my dreams"
+  ]
+
+  const result = glueRelated(input)
+  const expectedResult = [
+    "that is precicely the point you don't jump unless | obnoixnousness | foo",
+    "loving yourself is hard when you suck | when i leave my proven record",
+  ]
+
+  expect(
+    result
+  ).toEqual(expectedResult)
+})
---
+++ src/_helpers/glueRelated.ts
@@ -0,0 +1,71 @@
+
+import { sort, tail } from 'rambdax'
+
+const sorter = (a: string,b: string) => b.length - a.length
+const LIMIT = 75
+const SEP = ' | '
+
+function getTotalLength(words: string[]): number{
+  return words.reduce(
+    (prev, current) => prev + current.length,
+    0
+  )
+}
+
+function getLine(word: string, list: string[]): string[]{
+  let flag = true
+  let counter = -1
+  const answer = [word]
+
+  while(flag){
+    counter++
+
+    if(counter === list.length){
+      flag = false
+      continue
+    }
+    const possibleAnswer = [...answer, list[counter]]
+
+    const len = getTotalLength(possibleAnswer)
+    const separatorLength = (possibleAnswer.length - 1)*SEP.length
+    if(len + separatorLength < LIMIT){
+      answer.push(list[counter])
+    }
+  }
+
+  return answer
+}
+
+function glueRelated(words: string[]){
+  const filtered = words.filter(
+    x => x.length < LIMIT
+  )
+
+  const sorted = sort(sorter, filtered)
+  if(sorted.length <= 2){
+    return sorted
+  }
+
+  const firstLine = getLine(sorted[0], tail(sorted))
+  
+  const remain = sorted.filter(
+    x => !firstLine.includes(x)
+  )
+
+  if(remain.join(SEP).length < LIMIT){
+
+    return [
+      firstLine.join(SEP),
+      remain.join(SEP),
+    ]
+  }
+
+  const secondLine = getLine(remain[0], tail(remain))
+
+  return [
+    firstLine.join(SEP),
+    secondLine.join(SEP),
+  ]
+}
+
+exports.glueRelated = glueRelated
