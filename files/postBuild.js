3056f9f98c306a5a1fee71d0b795bab48f48da9e
Dejan Toteff <deyan8284@gmail.com>
2018-03-12 14:58:41 +0200

    chore: basic prod webpack4

--- files/postBuild.js
+++ files/postBuild.js
@@ -2,16 +2,17 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const sourcePublic = path.resolve(__dirname, '../dist')
-const sourceIndex = path.resolve(__dirname, '../dist/index.html')
 
 const outputPublic = path.resolve(__dirname, '../../server/dist/public')
-const outputIndex = path.resolve(__dirname, '../../server/dist/views/index.ejs')
+fs.copySync(sourcePublic, outputPublic, {overwrite: true })
 
+const sourceIndex = path.resolve(__dirname, '../../server/dist/public/index.html')
+// const sourceIndex = path.resolve(__dirname, '../dist/index.html')
+
+const outputIndex = path.resolve(__dirname, '../../server/dist/views/index.ejs')
 const outputSrcIndex = path.resolve(__dirname, '../../server/src/views/index.ejs')
 
-fs.copySync(sourcePublic, outputPublic, {overwrite: true })
 fs.copySync(sourceIndex, outputIndex, {overwrite: true })
-
 fs.copySync(sourceIndex, outputSrcIndex, {overwrite: true })
 
 console.log('done')
