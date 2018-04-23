4f4b413f1a06c262b5e0304e48abf1f0a5744caf
Dejan Toteff <deyan8284@gmail.com>
2018-04-23 15:58:24 +0300

    feat@progress: seo

--- files/seo.js
+++ files/seo.js
@@ -5,6 +5,9 @@ const {resolve} = require('path')
 const {readFileSync} = require('fs')
 const {pluck, prop} = require('rambdax')
 
+const SITE_TITLE = 'I Learn Smarter | English German Bulgarian Learning Apps'
+const SITE_DESCRIPTION = 'Language educational free web applications'
+
 const dbLocation = `${__dirname}/db.json`
 const rows = JSON.parse(readFileSync(dbLocation).toString()).rows
 const dbRaw = pluck('doc', rows)
@@ -42,9 +45,49 @@ function bodyStart(dbInstance){
   <header class="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
     <div class="wrap">
       <hgroup class="title-area">
-        <h1 class="site-title" itemprop="name">Site Title</h1>
-        <h2 class="site-description" itemprop="description">Site Description</h2>
+        <h1 class="site-title" itemprop="name">
+          ${SITE_TITLE}
+        </h1>
+        <h2 class="site-description" itemprop="description">
+          ${SITE_DESCRIPTION}
+        </h2>
       </hgroup>
     </div>
   </header>`
+}
+
+
+/**
+ * link to all apps, to contact
+ */
+function navigation(){
+
+  return `
+  <nav 
+    class="site-navbar" 
+    role="navigation" 
+    itemscope 
+    itemtype="http://schema.org/SiteNavigationElement"
+  >
+  <div class="wrap">
+    <ul class="site-navbar-nav">
+      <li>
+        <a 
+          itemprop="url" 
+          href="${URL}"
+        >
+          <span itemprop="name">Home</span>
+        </a>
+      </li>
+      <li>
+        <a 
+          itemprop="url" 
+          href="${URL}/about"
+        >
+          <span itemprop="name">About</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>`
 }
