46873ef79d33127e5a465ac9438587ccad48ba5f
Dejan Toteff <deyan8284@gmail.com>
2018-04-26 10:35:54 +0300

    chore@stop: seo | basic version is done

--- files/postBuild.js
+++ files/postBuild.js
@@ -1,11 +1,11 @@
-const fs = require('fs-extra')
+const {emptyDirSync, copySync} = require('fs-extra')
 const path = require('path')
 const {log} = require('log')
 const {editHTML} = require('./_helpers/editHTML')
 const {minify} = require('./_helpers/minify')
 
 log('minify start','info')
-minify()
+// minify()
 log('minify end', 'success')
 
 editHTML()
@@ -13,15 +13,28 @@ editHTML()
 const resolve = relativePath => path.resolve(__dirname, relativePath)
 
 log('move start','info')
-const dist = resolve('../dist')
 
 /**
  * move `./dist` folder to `server/public`
  */
 const serverPublic = resolve('../../server/dist/public')
-fs.removeSync(serverPublic)
-fs.copySync(dist, serverPublic, {overwrite: true })
+emptyDirSync(serverPublic)
 
+const dist = resolve('../dist')
+const seo = resolve('../files/seo')
+
+copySync(dist, seo, {overwrite: true })
+/**
+ * Move favicon
+ */
+const favicon = resolve('./favicon.ico')
+const seoFavicon = resolve('../files/seo/favicon.ico')
+
+copySync(favicon, seoFavicon, {overwrite: true })
+/**
+ * Move seo
+ */
+copySync(seo, serverPublic, {overwrite: true })
 /**
  * Move index.html to `server/views` folders
  */
@@ -29,17 +42,7 @@ const indexHTML = resolve('../../server/dist/public/index.html')
 const serverEJS = resolve('../../server/dist/views/index.ejs')
 const serverSrcEJS = resolve('../../server/src/views/index.ejs')
 
-fs.copySync(indexHTML, serverEJS, {overwrite: true })
-fs.copySync(indexHTML, serverSrcEJS, {overwrite: true })
-
-/**
- * Move favicon
- */
-const favicon = resolve('./favicon.ico')
-const serverFavicon = resolve('../../server/dist/public/favicon.ico')
-
-fs.copySync(favicon, serverFavicon, {overwrite: true })
-
-// move seo
+copySync(indexHTML, serverEJS, {overwrite: true })
+copySync(indexHTML, serverSrcEJS, {overwrite: true })
 
 log('move end', 'success')
--- files/seo.js
+++ files/seo.js
@@ -1,15 +1,16 @@
 /**
  * Following example HTML5 markup at https://gist.github.com/MilanAryal/ee861d7a065cc05868d9
  */
-// TODO: Prev and nav links 
-const {resolve} = require('path')
-const {readFileSync, writeFileSync} = require('fs')
-const {emptyDirSync} = require('fs-extra')
-const {pluck, prop, trim, take } = require('rambdax')
-const {kebabCase, titleCase} = require('string-fn')
+//TODO: Prev and nav links
+//TODO: CSS style
+const { emptyDirSync } = require('fs-extra')
+const { kebabCase, titleCase } = require('string-fn')
 const { minify } = require('html-minifier')
+const { pluck, prop, trim, take } = require('rambdax')
+const { readFileSync, writeFileSync } = require('fs')
+const { resolve } = require('path')
 
-const ID_HOLDER = `${__dirname}/_helpers/idHolder.js`
+const ID_HOLDER = `${ __dirname }/_helpers/idHolder.js`
 const SITE_TITLE = 'I Learn Smarter | English German Bulgarian Learning Apps'
 const SITE_DESCRIPTION = 'Language educational free web applications'
 const URL = 'https://ilearnsmarter.com'
@@ -17,41 +18,41 @@ const apps = [
   'learning.meme',
   'choose.word',
   'write.sentence',
-  'select.article', 
-  'guess.word'
+  'select.article',
+  'guess.word',
 ]
 
-const dbLocation = `${__dirname}/db.json`
+const dbLocation = `${ __dirname }/db.json`
 const rows = JSON.parse(readFileSync(dbLocation).toString()).rows
 const dbRaw = pluck('doc', rows)
 const db = dbRaw.filter(prop('imageSrc'))
 
-emptyDirSync(`${__dirname}/seo`)
+emptyDirSync(`${ __dirname }/seo`)
 
-void function seo(){
-  const idHolder = take(5, db).map(_ => {
+void function seo () {
+  const idHolder = take(10, db).map(_ => {
     const content = parseSingleInstance(_)
-    const destination = `${__dirname}/seo/${_._id}.html`
-    writeFileSync(destination, content) 
+    const destination = `${ __dirname }/seo/${ _._id }.html`
+    writeFileSync(destination, content)
+
     return _._id
   })
 
-  const idHolderContent = `exports.idHolder = ${JSON.stringify(idHolder)}`
+  const idHolderContent = `exports.idHolder = ${ JSON.stringify(idHolder) }`
   writeFileSync(ID_HOLDER, idHolderContent)
-  let c
 }()
 
-function parseSingleInstance(_){
-  const html = `${head(_)}${bodyStart(_)}${navigation(_)}${main(_)}`
+function parseSingleInstance (_) {
+  const html = `${ head(_) }${ bodyStart(_) }${ navigation(_) }${ main(_) }`
 
   return minify(html, {
-    trimCustomFragments: true,
-    removeAttributeQuotes: true
+    trimCustomFragments   : true,
+    removeAttributeQuotes : true,
   })
 }
 
-function head(dbInstance){
-  // <link rel="stylesheet" href="/seo.css">
+function head (dbInstance) {
+  //<link rel="stylesheet" href="/seo.css">
 
   return `
   <!DOCTYPE html>
@@ -60,22 +61,21 @@ function head(dbInstance){
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>${dbInstance.enPart} | I Learn Smarter</title>  
+      <title>${ dbInstance.enPart } | I Learn Smarter</title>  
     </head>`
 }
 
-function bodyStart(dbInstance){
-
+function bodyStart () {
   return `<body itemscope itemtype="http://schema.org/WebPage">
 
   <header class="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
     <div class="wrap">
       <hgroup class="title-area">
         <h1 class="site-title" itemprop="name">
-          ${SITE_TITLE}
+          ${ SITE_TITLE }
         </h1>
         <h2 class="site-description" itemprop="description">
-          ${SITE_DESCRIPTION}
+          ${ SITE_DESCRIPTION }
         </h2>
       </hgroup>
     </div>
@@ -85,14 +85,14 @@ function bodyStart(dbInstance){
 /**
  * link to all apps, to contact
  */
-function navigation(){
+function navigation () {
   const links = apps.map(_ => `
       <li>
         <a 
           itemprop="url" 
-          href="${URL}/${kebabCase(_)}"
+          href="${ URL }/${ kebabCase(_) }"
         >
-          <span itemprop="name">${titleCase(_)}</span>
+          <span itemprop="name">${ titleCase(_) }</span>
         </a>
       </li> 
   `).map(trim)
@@ -109,48 +109,47 @@ function navigation(){
       <li>
         <a 
           itemprop="url" 
-          href="${URL}"
+          href="${ URL }"
         >
           <span itemprop="name">Home</span>
         </a>
       </li>
-      ${links}
+      ${ links }
     </ul>
   </div>
 </nav>`
 }
 
-function createRelated(dbInstance, label){
-
-  const focusWord = dbInstance[`${label}Word`]
-  const related = dbInstance[`${label}Related`].join(',')
+function createRelated (dbInstance, label) {
+  const focusWord = dbInstance[ `${ label }Word` ]
+  const related = dbInstance[ `${ label }Related` ].join(',')
 
   return `
-  <p class="text ${label}related">
-    Related words for "${focusWord}": ${related}
+  <p class="text ${ label }related">
+    Related words for "${ focusWord }": ${ related }
   </p>
   `
 }
 
-function main(dbInstance){
+function main (dbInstance) {
   const bgPart = dbInstance.bgPart ?
-    `<p class="text bgpart">Bulgarian translation: ${dbInstance.bgPart}</p>` :
+    `<p class="text bgpart">Bulgarian translation: ${ dbInstance.bgPart }</p>` :
     ''
 
   const bgWord = dbInstance.bgWord ?
-    `<p class="text bgword">Bulgarian word on focus: ${dbInstance.bgWord}</p>` :
+    `<p class="text bgword">Bulgarian word on focus: ${ dbInstance.bgWord }</p>` :
     ''
 
   const enRelated = dbInstance.enRelated && dbInstance.enRelated.length > 0 ?
-    createRelated(dbInstance, 'en') : 
+    createRelated(dbInstance, 'en') :
     ''
 
   const deRelated = dbInstance.deRelated && dbInstance.deRelated.length > 0 ?
-    createRelated(dbInstance, 'de') : 
+    createRelated(dbInstance, 'de') :
     ''
 
   const bgRelated = dbInstance.bgRelated && dbInstance.bgRelated.length > 0 ?
-    createRelated(dbInstance, 'bg') : 
+    createRelated(dbInstance, 'bg') :
     ''
 
   return `
@@ -159,32 +158,32 @@ function main(dbInstance){
     <article class="post" itemscope itemtype="http://schema.org/Article">
       <header class="post-header">
         <h3 class="post-title" itemprop="name headline">
-          <a href="${URL}/${dbInstance._id}">
-            ${dbInstance.enPart}
+          <a href="${ URL }/${ dbInstance._id }">
+            ${ dbInstance.enPart }
           </a>
         </h3>
       </header>
 
       <div class="post-content" itemprop="articleBody">
         <p class="text depart">
-          German translation: ${dbInstance.dePart}
+          German translation: ${ dbInstance.dePart }
         </p>
-        ${bgPart}
+        ${ bgPart }
         <p class="text enword">
-          English word on focus: ${dbInstance.enWord}
+          English word on focus: ${ dbInstance.enWord }
         </p>
-        ${enRelated}
+        ${ enRelated }
         <p class="text deword">
-          German word on focus: ${dbInstance.deWord}
+          German word on focus: ${ dbInstance.deWord }
         </p>
-        ${deRelated}
-        ${bgWord}
-        ${bgRelated}
+        ${ deRelated }
+        ${ bgWord }
+        ${ bgRelated }
         <hr />
         <div class="image">
           <img 
-            alt="${dbInstance.altTag}"
-            src="${dbInstance.imageSrc}"
+            alt="${ dbInstance.altTag }"
+            src="${ dbInstance.imageSrc }"
           />  
         </div>
       </div>
