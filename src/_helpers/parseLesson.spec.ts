81e127998a6edf26042c94a86a84ee403b317b11
Dejan Toteff <deyan8284@gmail.com>
2018-11-03 21:33:41 +0200

    feat: parse.lesson method

---
+++ src/_helpers/parseLesson.spec.ts
@@ -0,0 +1,36 @@
+import { pass } from 'rambdax'
+import { parseLesson } from './parseLesson'
+
+const input = `
+# Third person
+
+Трето лице **единствено число**
+---
+# Пример 1
+
+Kate has[have][had] a nice[polite][expensive] smile.
+
+> Кейт има хубава усмивка.
+---
+# Кога завършваме с 's'
+
+description of why we have such rules
+It can contain a list:
+
+- foo
+- bar
+---
+# Пример 2
+
+Kate used[had][need] to smoke but now she runs[run][ran] a lot instead.
+`
+
+test('', () =>{
+  const result = parseLesson(input)
+  
+  result.forEach(x => {
+    const okExample = pass(x)({title:'string', example:'string', translation:'string'})
+    const okContent = pass(x)({title:'string', text:'string'})
+    expect(okExample || okContent).toBeTruthy()
+  })
+})
---
+++ src/_helpers/parseLesson.ts
@@ -0,0 +1,61 @@
+import { join, init, ifElse, startsWith, nth, remove, filter, trim, head, piped, split, always } from 'rambdax'
+
+const getTitle = x => piped(
+  x,
+  split('\n'),
+  filter(Boolean),
+  head,
+  remove('#'),
+  trim
+)
+
+const getContent = x => piped(
+  x,
+  split('\n'),
+  filter(Boolean),
+  init,
+  join('\n')
+)
+
+const getExample = x => piped(
+  x,
+  split('\n'),
+  filter(Boolean),
+  nth(1),
+  trim
+)
+
+const getTranslation = x => piped(
+  x,
+  split('\n'),
+  filter(Boolean),
+  filter(startsWith('>')),
+  ifElse(
+    x => x.length > 0, 
+    x => remove('> ', head(x)),
+    always('')
+  )
+)
+
+export function parseLesson(text: any){
+  const parts = text.split('---')
+
+  return parts.map(x => {
+    const title = getTitle(x)
+    if(x.includes('[') && x.includes(']')){
+      const example = getExample(x)
+      const translation = getTranslation(x)
+
+      return {
+        title, 
+        example, 
+        translation
+      }
+    }
+
+    return {
+      title,
+      text: getContent(x)
+    }
+  })
+}
