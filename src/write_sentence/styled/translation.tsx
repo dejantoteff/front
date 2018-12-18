9c09f45b959899995ff91867ab56b5c8dfd5b4a8
Dejan Toteff <deyan8284@gmail.com>
2018-12-18 08:37:58 +0200

    feat@style: text shaddow on write.sentence transllation

--- src/write_sentence/styled/translation.tsx
+++ src/write_sentence/styled/translation.tsx
@@ -1,17 +1,28 @@
-import { light7 } from 'colors'
+import { light7, dark2 } from 'colors'
 import styled from 'styled-components'
 import { CenteredItem } from '../../_styled/grid'
 import { getText, Text } from './grid'
+import { glue } from 'rambdax'
 
 export const TranslationContainer = styled(CenteredItem)`
   grid-area: ws_translation;
   width: 100%;            
 `
 
+const textShaddow = glue(`
+  text-shadow:
+  -1px -1px 0 ${dark2},
+  1px -1px 0 ${dark2},
+  -1px 1px 0 ${dark2},
+  1px 1px 0 ${dark2};
+`)
+
 export const Translation =  styled(Text)`
   color: ${light7};
+  ${textShaddow}
 `
 
 export const TranslationSmall = styled(getText(0.37))`
   color: ${light7};
+  ${textShaddow}
 `
