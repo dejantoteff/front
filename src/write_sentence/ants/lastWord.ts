b3e9d3d1ac53e3bec39924360ef2e04edad5e2df
Dejan Toteff <deyan8284@gmail.com>
2018-12-27 23:34:18 +0200

    feat@write.sentence: reload on navigation from auto mode

--- src/write_sentence/ants/lastWord.ts
+++ src/write_sentence/ants/lastWord.ts
@@ -1,5 +1,9 @@
-export function lastWordAnt(){
+export function lastWordAnt(): any{
   const container = document.getElementById('ws_answer')
+  // Happens only if from auto.mode navigate somewhare else
+  ///////////////////////////
+  if(container === null) return window.location.reload(false)
+
   const last = container.querySelectorAll('span:last-child')[0]
 
   return window.getComputedStyle(last).visibility === 'visible'
